7b1ed74559b7965411c24d676c527cf92571031d
Donal Sheehan <dsheehan@dsheehan-ltm1.internal.salesforce.com>
2016-04-22 11:18:23 +0100

    checkhosts v1

---
+++ bin/check_hosts.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""Script to validate which os is being worked on"""
+
+#from datetime import datetime, date, timedelta
+import sys
+import re
+import subprocess
+import logging
+from optparse import OptionParser
+#import glob
+#import common
+#import json
+import os.path
+#import platform
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+if __name__ == "__main__":
+    usage = """
+    Validate the hosts OS type.
+    
+    %prog -t [rhel|centos|oel|solaris] -v
+    
+    %prog -t rhel
+    
+    
+    """
+    parser = OptionParser(usage)
+    parser.add_option("-H", dest="hosts", action="store", help="comma separated list of hosts , buildplan v_HOSTS")
+    parser.add_option("-v", action="store_true", dest="verbose", help="verbosity")
+    (options, args) = parser.parse_args()
+    
+
+    def cleanline(line):
+        return line.strip('\n')
+ 
+    if options.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    filename =  os.path.expanduser('~') + '/checkhosts'
+    excludelist, returnlist = [],[]
+    with open(filename,'r') as f:
+        for line in f:
+	    excludelist.append(cleanline(line))
+    for line in options.hosts.split(','):
+        line = cleanline(line)
+        if line not in excludelist:
+            returnlist.append(line)
+            
+    logging.debug('Excludelist:') 
+    logging.debug(excludelist) 
+    logging.debug('Returnlist:') 
+    logging.debug(returnlist) 
+    
+    print ','.join(returnlist)
+    #if options.hostlist:
+        #if arch == 'Linux':
+        #    os = whichOS()
+        #    if os == options.ostype.upper():
+        #        print("Checked OS matches host OS : %s : %s" % (os, options.ostype.upper()))
+        #        sys.exit(0)
+        #    else:
+        #        print("Checked OS does not match host OS : %s : %s" % (os, options.ostype.upper()))
+        #        sys.exit(1)
+        #elif arch == 'SunOs':
+        #    os = 'Solaris'
+        #else:
+        #    os = 'Unknown'
