6d4a497793810f213bc7c1e1aa3aa539438bb69a
Mitchell Gaddy <mgaddy@salesforce.com>
2016-09-29 21:32:31 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-install_dir = '/opt/cpt/bin'
-install_dir2 = '/opt/cpt/modules'
-install_files = ['bin/check_hosts.py',
+bin_dir = '/opt/cpt/bin'
+modules_dir = '/opt/cpt/modules'
+bin_files = ['bin/check_hosts.py',
                  'bin/check_local_port.py',
                  'bin/check_maxfs_desc.py',
                  'bin/check_mq_buddy.py',
@@ -19,10 +19,10 @@ install_files = ['bin/check_hosts.py',
                  'bin/verify_search_buddy.py',
                  'bin/zookeeper_status.py',
                  'cptops_nagios/bin/nagios_monitor.py',
-                 'cptops_nagios/bin/nagios_monitor_single.py']
+                 'cptops_nagios/bin/nagios_monitor_single.py',
+                 'cptops_idbhost/includes/idbhost.py']
 
-install_files2 = ['idbhost/includes/idbhost.py',
-                  'idbhost/includes/common.py']
+modules_files = ['idbhost/includes/common.py']
 
 setup(
     name='cpt-tools',
@@ -32,8 +32,8 @@ setup(
     author_email='mgaddy@salesforce.com',
     url="https://git.soma.salesforce.com/CPT/cptops_validation_tools",
     packages=find_packages(),
-    data_files=[(install_dir, install_files),
-                (install_dir2, install_files2)],
+    data_files=[(bin_dir, bin_files),
+                (modules_dir, modules_files)],
     classifiers=[
           "License :: Salesforce Proprietary Code",
           "Programming Language :: Python",
