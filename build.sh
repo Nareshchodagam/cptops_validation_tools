1cf449de20b1e98eea7463dfb37679bf63d37300
Mitchell Gaddy <mgaddy@salesforce.com>
2016-11-01 13:23:21 -0400

    Update build.sh to include sysfiles to the rpm.  Updated setup.py to include files from the cptops_sysfiles repo.
     Update manage_service.py to handle applications that are controlled by
    init. It will use the service command rather than having to type out the
    actual command. The -i options specifies the command is controlled by
    init.

--- build.sh
+++ build.sh
@@ -55,6 +55,7 @@ fi
 #Clone other git repos
 git clone git@git.soma.salesforce.com:CPT/cptops_idbhost $WORKSPACE/cptops_idbhost -b master
 git clone git@git.soma.salesforce.com:CPT/cptops_nagios $WORKSPACE/cptops_nagios -b master
+git clone git@git.soma.salesforce.com:CPT/cptops_sysfiles.git $WORKSPACE/cptops_sysfiles -b master
 git clone --recursive git@git.soma.salesforce.com:CPT/cptops_exec_with_creds $WORKSPACE/cptops_exec_with_creds -b master 
 
 fpm -s python -t rpm \
