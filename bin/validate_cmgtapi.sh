dbf0fdb5a50fbca0a46ae93ccfcdaa780ae56df1
wpiras <wpiras@wpiras-ltm1.internal.salesforce.com>
2017-09-22 17:51:12 -0700

    Fixed a minor issue

--- bin/validate_cmgtapi.sh
+++ bin/validate_cmgtapi.sh
@@ -9,6 +9,6 @@ if [ ${RETVAL1} -eq 0 ]
 		# If the previous script returned 0, CSApi is already running	
 		echo "CSApi Processes:        [RUNNING]" 
 	else
-		# If CSApi is not running, start it
-      	echo "ERROR CSApi Processes:        [NOT RUNNING]" 
+		# CSApi is not running
+		echo "ERROR CSApi Processes:        [NOT RUNNING]" 
 fi
