8c0584bbcf89d65bdf85d914d129bdbb1f789f22
robert-murray <robert.murray@salesforce.com>
2018-01-25 15:40:56 +0000

    fixed validate_vnscan.sh and added new validate_vnscanam.sh

--- bin/validate_vnscan.sh
+++ bin/validate_vnscan.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#$Id: validate_vnscan.sh 242300 2016-12-02 15:14:12Z iviscio@SFDC.NET $
-if [ -f /etc/rc.d/init.d/nexposeengine.rc ]
+#$Id: validate_vnscan.sh 242300 2018-1-02 15:14:12Z rmurray@SFDC.NET $
+if [ -f /etc/rc.d/init.d/nessusd ]
 then
-	STATUS=$(/etc/rc.d/init.d/nexposeengine.rc status)
-	if [ "${STATUS}" == "NeXpose security engine is running." ]
-	        then
-	        echo "NeXpose security engine:        [RUNNING]"
-	else
-	        echo "ERROR NeXpose security engine:        [NOT RUNNING]"
-	        exit 1
-	fi
-else 
-        echo "ERROR NeXpose Not Installed"
+      /bin/ps -ef | grep nessusd | grep -v grep
+                if [ $? -eq 0 ]
+                then
+                echo "Nessus Scanner :        [RUNNING]"
+        else
+                echo "ERROR Nessus Scanner :        [NOT RUNNING]"
+                exit 1
+        fi
+else
+        echo "ERROR Nessus Scanner Not Installed"
         exit 1
 fi
