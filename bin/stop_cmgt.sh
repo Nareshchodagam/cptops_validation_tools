b91e7c49f430eb98ff5ed59d34dbffa53d61f8aa
wpiras <wpiras@wpiras-ltm1.sfdcgomobile.salesforce.com>
2018-03-15 12:37:47 -0700

    Updated CPT scripts to include preprocessor changes

--- bin/stop_cmgt.sh
+++ bin/stop_cmgt.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 # $Id $
 
+####################################################################################
+#
+# Please contact the Certificate Service group for any info about this script
+# https://gus.my.salesforce.com/0F9B00000006LcG
+# 
+# To test this script in the Cert Service DEV infrastructure just define CMGT_ROOT
+# and CMGT_PREPROCESSOR_ROOT to point to the tested services instances. 
+#
+####################################################################################
 
-PIDFILE="/home/csbroker/cs_request_processor-data/cs_request_processor.pid"
+CMGT_ROOT="/home/csbroker"
+CMGT_PREPROCESSOR_ROOT="/home/csbroker"
+
+PIDFILE="${CMGT_ROOT}/cs_request_processor-data/cs_request_processor.pid"
+PIDFILE_PREPROCESSOR="${CMGT_PREPROCESSOR_ROOT}/cs_request_preprocessor-data/cs_request_preprocessor.pid"
+
+function stop_preprocessor {
+su - csbroker -c "cd ${CMGT_PREPROCESSOR_ROOT}/cs_request_preprocessor && bin/cs_preprocessor.sh stop"
+echo "CS Preprocessor Processes:        [STOPPED]"
+}
 
 function stop_broker {
-su - csbroker -c "cd /home/csbroker/cs_request_processor && bin/cs_processor.sh stop"
+su - csbroker -c "cd ${CMGT_ROOT}/cs_request_processor && bin/cs_processor.sh stop"
 echo "CS Broker Processes:        [STOPPED]"
 }
 
@@ -14,6 +32,15 @@ if [ -f ${PIDFILE} ]
 	  #if pidfile exists stop CS Broker
 	  stop_broker
 else
-   echo "CS BROKER NOT RUNNING"
+   echo "ERROR CS Broker Processes:        [NOT RUNNING]"
 fi
+
+if [ -f ${PIDFILE_PREPROCESSOR} ] 
+  then
+      #if pidfile exists stop CS Preprocessor
+      stop_preprocessor
+else
+   echo "ERROR CS Preprocessors Processes:        [NOT RUNNING]"
+fi
+
 
