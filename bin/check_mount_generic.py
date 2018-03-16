b774fc448e0361adc043c9e07ec4fc6c9408ca6c
mgaddy <mgaddy@salesforce.com>
2018-03-15 23:00:54 -0400

    Updates check_mount_generic
    Script to check for NFS mount to finish before starting app. Used in the artifactrepo.

--- bin/check_mount_generic.py
+++ bin/check_mount_generic.py
@@ -5,6 +5,9 @@ import logging
 import re
 import shlex
 from subprocess import Popen,PIPE
+import sys
+import os
+import time
 
 
 def check_mtab(mount_point):
@@ -25,33 +28,51 @@ def check_fstab(mount_point):
             for line in f:
                 if "%s" % (mount_point) in line and not line.startswith('#'):
                     print("%s mount point is present in fstab" % (mount_point))
-                    cmd = shlex.split("mount %s" % (mount_point))
-                    p = Popen(cmd, stdout=PIPE)
-                    (out, err) = p.communicate()
-                    print(out)
-                    rtrn_code = p.returncode
-                    return True
-                    break
-                else:
-                    continue
-            return False
 
 
-
+def mount_filesystem(mount_point):
+    '''
+    Function to mount the filesystem.
+    :param mount_point:
+    :return:
+    '''
+    cmd = shlex.split("mount %s" % (mount_point))
+    p = Popen(cmd, stdout=PIPE)
+    (out, err) = p.communicate()
+    print(out)
+    rtrn_code = p.returncode
+    if rtrn_code != 0:
+        print("Failed to mount {} filesystem.").format(mount_point)
+        sys.exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Verify the mount points')
-    parser.add_argument("-m", dest="mount", help="Mount point name")
+    parser.add_argument("-f", dest="filesystem", help="Filesystem to check")
+    parser.add_argument("-m", action="store_true", dest="mount", help="Perform a mount.")
+    parser.add_argument("-c", "--check", action="store_true", dest="check", help="Check mount")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="verbose")
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    if args.mount:
-        rtrn = check_fstab(args.mount)
+    if args.check and not args.filesystem:
+        print("Missing mounpoint to check")
+        sys.exit(1)
+    elif args.check and args.filesystem:
+        counter = 0
+        while (counter != 3):
+            if os.path.ismount(args.filesystem):
+                print("{} filesystem is mounted").format(args.filesystem)
+                sys.exit(0)
+            counter += 1
+            time.sleep(60)
+        sys.exit(1)
+
+    if args.filesystem:
+        rtrn = check_fstab(args.filesystem)
         if rtrn is not True:
             print("May be someone has modify the fstab file, please check manually")
             exit(1)
         else:
-            print("Mounted the %s" % args.mount)
+            print("Mounted the %s" % args.filesystem)
             exit(0)
