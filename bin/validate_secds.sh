162c5b0c87461a152b6907f652695d1fddc7dc0c
Sylvia Nolan <snolan@salesforce.com>
2018-12-05 11:03:48 +0000

    Update secds and seciamsvc validate scripts to add wait and service not deployed condition - W-5671916

--- bin/validate_secds.sh
+++ bin/validate_secds.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
+# validate_secds.sh 2018-12-05 snolan@SFDC.NET
 
-CHECK_SVC=$(systemctl is-active --quiet secds); RETVAL=$?
+function running {
 
+sleep 10
+n=0
+until [ $n -ge 3 ]
+do
+ systemctl is-active --quiet secds
+  if [ $? -eq 0 ]
+  then
+        RETVAL1=0
+        echo "SECDS: [RUNNING]"
+        exit 0
+  else
+         RETVAL1=1
+         echo "${HOSTNAME}:SECDS: [DEPLOYED but not RUNNING]. Checking again in 1 min"
+         n=$[$n+1]
+         sleep 60
+  fi
+done
+
+[ "$RETVAL1" -eq 1 ] && echo "${HOSTNAME}: SECDS: [FAILED TO START]. Contact @SecDS - Secured Directory Services and @Hardening"
+exit 1
+
+}
+
+
+RPM_CHECK=$(rpm -qa | grep secds); RETVAL=$?
 if [ ${RETVAL} -eq 0 ]
         then
-        echo "secds Processes:        [RUNNING]"
+        running
 else
-        echo "ERROR secds Processes:        [NOT RUNNING]"
-        exit 1
+        echo "SECDS:  [NOT DEPLOYED - skipping check] "
+        exit 0
 fi
